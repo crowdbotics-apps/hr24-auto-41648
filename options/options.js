3f4e0d243270be5819ea70d7334b4e5bdf73ea04
Ramon Williams Jr <ramon.williams.jr@gmail.com>
2023-06-21 16:01:46 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "hr24-auto-41648",
   url: "https://hr24-auto-41648.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
