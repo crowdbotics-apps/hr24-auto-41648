3efc3ae0c3831ff5366210e7dd062de71c5e3999
Crowdbotics <team@crowdbotics.com>
2023-06-21 06:14:57 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hr24-auto-41648",
+  url: "https://hr24-auto-41648.botics.co",
+  api: "https://hr24-auto-41648.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
